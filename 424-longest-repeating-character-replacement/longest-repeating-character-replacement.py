402856d09de48a85d640902e26627cd2ddfee71f
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-08-12 21:58:48 +0530

    Time: 113 ms (87.10%) | Memory: 15.4 MB (72.95%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        from collections import defaultdict
+        
+        count = defaultdict(int)
+        max_freq=0
+        left=0
+        max_len=0
+
+        for right in range(len(s)):
+            count[s[right]]+=1
+            max_freq=max(max_freq,count[s[right]])
+            if (right - left + 1)- max_freq > k:
+                count[s[left]]-=1
+                left+=1
+            max_len=max(max_len,right-left+1)
+        return max_len
