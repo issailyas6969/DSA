9107187b71bd4af833fa7ec8eaa8b80b8a563e6c
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-08-17 03:33:26 +0530

    Time: 40 ms (20.95%) | Memory: 12.7 MB (57.42%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return None
+        oldtonew={}
+        def clone(node):
+            if node in oldtonew:
+                return oldtonew[node]
+            copy=Node(node.val)
+            oldtonew[node]=copy
+            for nei in node.neighbors:
+                copy.neighbors.append(clone(nei))
+            return copy        
+        return clone(node)
