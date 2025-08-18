031e6af0962d017becff3f7e7715d2d3a2af5402
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-08-18 22:50:59 +0530

    Time: 11 ms (48.30%) | Memory: 16.2 MB (19.26%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        premap={i:[] for i in range(numCourses)}
+        for crs,pre in prerequisites:
+            premap[crs].append(pre)
+        
+        visited=set()
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if premap[crs]==[]:
+                return True
+            visited.add(crs)
+            for pre in premap[crs]:
+                if not dfs(pre):return False
+            visited.remove(crs)
+            premap[crs]=[]
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs):return False
+        return True
