f8007df134997c0480bd9d5af5f7e4d19e9a97d5
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-09-23 00:59:17 +0530

    Time: 114 ms (27.56%) | Memory: 13.6 MB (32.76%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -8,16 +8,16 @@ class Solution(object):
         res=[]
         from collections import Counter
         n,m=len(p),len(s)
-        count_p=Counter(p)
+        cp=Counter(p)
         win=Counter(s[:n])
-        if count_p==win:
+        if cp==win:
             res.append(0)
         for i in range(n,m):
             win[s[i]]+=1
             win[s[i-n]]-=1
             if win[s[i-n]]==0:
                 del win[s[i-n]]
-            if win==count_p:
+            if cp==win:
                 res.append(i-n+1)
         return res
 
