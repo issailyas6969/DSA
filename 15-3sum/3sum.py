4f56477ff81d54363cc12d8e43d6b349e81a41a5
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-11-15 22:51:39 +0530

    Time: 723 ms (58.36%) | Memory: 18.2 MB (88.06%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -7,12 +7,12 @@ class Solution(object):
         nums.sort()
         res=[]
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i>0 and nums[i]==nums[i-1]:
                 continue
             l,r=i+1,len(nums)-1
             while l<r:
-                t=nums[i]+nums[l]+nums[r]
-                if t==0:
+                tot=nums[i]+nums[l]+nums[r]
+                if tot==0:
                     res.append([nums[i],nums[l],nums[r]])
                     while l<r and nums[l]==nums[l+1]:
                         l+=1
@@ -20,7 +20,7 @@ class Solution(object):
                         r-=1
                     l+=1
                     r-=1
-                elif t>0:
+                elif tot>0:
                     r-=1
                 else:
                     l+=1
