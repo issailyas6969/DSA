50824dd018887eda097c2348904737bc4be348a2
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-11-17 21:19:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (84.25%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -9,7 +9,7 @@ class Solution(object):
         """
         p1=m-1
         p2=n-1
-        p=m+n-1
+        p=len(nums1)-1
         while p1>=0 and p2>=0:
             if nums1[p1]>nums2[p2]:
                 nums1[p]=nums1[p1]
