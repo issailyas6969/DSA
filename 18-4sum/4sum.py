dc1b06e5724c5fb25e3059332f3a7270246d4abf
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-11-18 02:11:59 +0530

    Time: 515 ms (49.85%) | Memory: 12.5 MB (25.11%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -13,19 +13,19 @@ class Solution(object):
             for j in range(i+1,len(nums)-2):
                 if j>i+1 and nums[j]==nums[j-1]:
                     continue
-                left,right=j+1,len(nums)-1
-                while left<right:
-                    tot=nums[i]+nums[j]+nums[left]+nums[right]
+                l,r=j+1,len(nums)-1
+                while l<r:
+                    tot=nums[i]+nums[j]+nums[l]+nums[r]
                     if tot==target:
-                        res.append([nums[i],nums[j],nums[left],nums[right]])
-                        left+=1
-                        right-=1
-                        while left<right and nums[left]==nums[left-1]:
-                            left+=1
-                        while left<right and nums[right]==nums[right+1]:
-                            right-=1
-                    elif tot<target:
-                        left+=1
+                        res.append([nums[i],nums[j],nums[l],nums[r]])
+                        l+=1
+                        r-=1
+                        while l<r and nums[l]==nums[l-1]:
+                            l+=1
+                        while l<r and nums[r]==nums[r+1]:
+                            r-=1
+                    elif tot>target:
+                        r-=1
                     else:
-                        right-=1
+                        l+=1
         return res
