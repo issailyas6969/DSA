c9725cafa50a7f3087f8cc2bf509cdeb02742767
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-11-24 01:42:48 +0530

    Time: 4 ms (37.23%) | Memory: 13.1 MB (60.17%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res=[]
+        for i in nums:
+            if i%2==0:
+                res.append(i)
+        for j in nums:
+            if j%2!=0:
+                res.append(j)
+        return res
+        
