47fd9d7e4d3972d27cb63e896dc9aa14c9e75184
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-11-29 02:03:35 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (34.36%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left,right=0,len(nums)-1
-        while left<right:
-            mid=(left+right)//2
-            if mid%2==1:
+        l,r=0,len(nums)-1
+        while l<r:
+            mid=(l+r)//2
+            if mid%2:
                 mid-=1
             if nums[mid]==nums[mid+1]:
-                left=mid+2
+                l=mid+2
             else:
-                right=mid
-        return nums[left]
+                r=mid
+        return nums[l]
