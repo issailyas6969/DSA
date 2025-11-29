5c603fe630ef8a3bb00c94cb8fbe7690865fd185
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-11-30 03:22:19 +0530

    Time: 115 ms (86.64%) | Memory: 15.4 MB (69.29%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -7,9 +7,9 @@ class Solution(object):
         """
         from collections import defaultdict
         count=defaultdict(int)
+        left=0
         max_freq=0
         max_len=0
-        left=0
         for right in range(len(s)):
             count[s[right]]+=1
             max_freq=max(max_freq,count[s[right]])
@@ -17,5 +17,6 @@ class Solution(object):
                 count[s[left]]-=1
                 left+=1
             max_len=max(max_len,right-left+1)
-        return max_len
+        return max_len            
+            
         
