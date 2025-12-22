9ed82b185032cd2f909ae94f59bf9cb23d6a1a59
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-12-23 02:46:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (96.53%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -16,7 +16,7 @@ class Solution(object):
                 else:
                     left=mid+1
             else:
-                if nums[right]>=target>nums[mid]:
+                if nums[mid]<target<=nums[right]:
                     left=mid+1
                 else:
                     right=mid-1
