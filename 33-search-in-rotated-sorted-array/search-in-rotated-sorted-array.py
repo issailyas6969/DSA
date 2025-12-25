2e5ea2ffeef1c1fc872ecbf4f05bd0fa0f82bad3
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-12-26 03:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (70.44%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,20 +5,19 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        left,right=0,len(nums)-1
-        while left<=right:
-            mid=(left+right)//2
-            if nums[mid]==target:
-                return mid
-            if nums[left]<=nums[mid]:
-                if nums[left]<=target<nums[mid]:
-                    right=mid-1
+        l,r=0,len(nums)-1
+        while l<=r:
+            m=(l+r)//2
+            if nums[m]==target:
+                return m
+            if nums[l]<=nums[m]:
+                if nums[l]<=target<nums[m]:
+                    r=m-1
                 else:
-                    left=mid+1
+                    l=m+1
             else:
-                if nums[mid]<target<=nums[right]:
-                    left=mid+1
+                if nums[m]<target<=nums[r]:
+                    l=m+1
                 else:
-                    right=mid-1
+                    r=m-1
         return -1
-        
