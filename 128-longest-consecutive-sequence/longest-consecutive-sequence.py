5aeaab436f09bc2699a12b10bf2470d2f9dc4d83
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2025-12-27 01:31:40 +0530

    Time: 46 ms (88.36%) | Memory: 27 MB (44.75%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -10,10 +10,11 @@ class Solution(object):
             if num-1 not in num_set:
                 current=num
                 count=1
-
+                
                 while current+1 in num_set:
                     current+=1
                     count+=1
                 longest=max(longest,count)
         return longest
+             
         
