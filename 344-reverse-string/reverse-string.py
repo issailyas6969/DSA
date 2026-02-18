8869213e35917bd5d19221dbb091f99074040910
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2026-02-19 02:57:56 +0530

    Time: 8 ms (11.33%) | Memory: 19.9 MB (71.69%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -4,9 +4,10 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        left,right=0,len(s)-1
-        while left<right:
-            s[left],s[right]=s[right],s[left]
-            left+=1
-            right-=1
+        l,r=0,len(s)-1
+        while l<r:
+            s[l],s[r]=s[r],s[l]
+            l+=1
+            r-=1
+        
         
