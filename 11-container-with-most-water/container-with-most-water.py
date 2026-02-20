1f4d73827383d45e5a526f54033f4bcc6a43d849
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2026-02-20 06:27:11 +0530

    Time: 127 ms (16.90%) | Memory: 20.8 MB (21.57%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -5,14 +5,15 @@ class Solution(object):
         :rtype: int
         """
         l,r=0,len(height)-1
-        max_area=0
+        area=0
         while l<r:
             width=r-l
             hei=min(height[l],height[r])
-            max_area=max(max_area,hei*width)
+            area=max(area,width*hei)
             if height[l]<height[r]:
                 l+=1
             else:
                 r-=1
-        return max_area
+        return area
+
         
