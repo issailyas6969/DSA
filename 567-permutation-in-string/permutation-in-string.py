49d7bf93e3a48762381331e91895cfc3d7ba375b
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2026-03-04 22:24:36 +0530

    Time: 47 ms (33.04%) | Memory: 12.7 MB (86.83%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -9,15 +9,16 @@ class Solution(object):
         n,m=len(s1),len(s2)
         if n>m:
             return False
-        s1_c=Counter(s1)
+        c_s1=Counter(s1)
         win=Counter(s2[:n])
-        if win==s1_c:
+        if win==c_s1:
             return True
         for i in range(n,m):
             win[s2[i]]+=1
             win[s2[i-n]]-=1
             if win[s2[i-n]]==0:
                 del win[s2[i-n]]
-            if win==s1_c:
+            if win==c_s1:
                 return True
-        return False       
+        return False
+        
