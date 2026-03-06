a3b51a761b64ac8dcf14d677e9229fed5b859f52
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2026-03-06 06:07:56 +0530

    Time: 63 ms (11.43%) | Memory: 13.9 MB (87.41%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-        max_so_far = -1
-        for i in range(len(arr) - 1, -1, -1):
+        max_so_far= -1
+        for i in range(len(arr)-1,-1,-1):
             current=arr[i]
             arr[i]=max_so_far
             max_so_far=max(max_so_far,current)
-
         return arr
+        
