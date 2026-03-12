df2055600a2cedc3c433398a718adeafd72a0ac5
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2026-03-13 04:11:23 +0530

    Time: 23 ms (62.92%) | Memory: 13.2 MB (60.48%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,12 +5,13 @@ class Solution(object):
         :rtype: int
         """
         count_s=set()
-        maxx=0
         l=0
+        longest=0
         for r in range(len(s)):
             while s[r] in count_s:
                 count_s.remove(s[l])
                 l+=1
             count_s.add(s[r])
-            maxx=max(maxx,r-l+1)
-        return maxx
+            longest=max(longest,r-l+1)
+        return longest
+        
