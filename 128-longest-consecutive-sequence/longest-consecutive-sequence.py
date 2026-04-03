3fc16f414fb3b4f394f0e72332f50ab4889ac36c
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2026-04-04 04:01:24 +0530

    Time: 47 ms (86.66%) | Memory: 27.1 MB (32.38%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -10,6 +10,7 @@ class Solution(object):
             if num-1 not in nums_set:
                 current=num
                 count=1
+
                 while current+1 in nums_set:
                     current+=1
                     count+=1
