fc4dfcd0797ef6063ef47a9dd5ad4bb9b0c46558
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2026-04-10 03:26:27 +0530

    Time: 11 ms (89.59%) | Memory: 12.3 MB (68.51%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -6,12 +6,11 @@ class Solution(object):
         :rtype: str
         """
         res=[]
-        i,j=0,0
-        while i<len(word1) and j<len(word2):
-            res.append(word1[i])
-            res.append(word2[j])
-            i+=1
-            j+=1
-        res.extend(word1[i:])
-        res.extend(word2[j:])
+        i=0
+        for i in range(max(len(word1),len(word2))):
+            if i<len(word1):
+                res.append(word1[i])
+            if i<len(word2):
+                res.append(word2[i])
         return "".join(res)
+        
