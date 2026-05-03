d0b0bed8f62f46bc1d14062b2a4ca1b14ad0387b
issailyas6969 <163906542+issailyas6969@users.noreply.github.com>
2026-05-03 16:19:49 +0530

    Time: 20 ms (73.58%) | Memory: 13.1 MB (67.05%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        count_s=set()
-        l=0
+        char_set=set()
+        left=0
         longest=0
-        for r in range(len(s)):
-            while s[r] in count_s:
-                count_s.remove(s[l])
-                l+=1
-            count_s.add(s[r])
-            longest=max(longest,r-l+1)
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left+=1
+            char_set.add(s[right])
+            longest=max(longest,right-left+1)
         return longest
